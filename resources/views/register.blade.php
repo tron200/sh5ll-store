856a57d3baa0ede442cc4cb829cf6496b83d8045
Abdelrhman Mohamed Ezz-Eldin <62518324+tron200@users.noreply.github.com>
2023-02-13 17:28:04 +0200

    first commit

---
+++ resources/views/register.blade.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+    <title>Register</title>
+    <link rel="stylesheet" href="/css/auth.css">
+</head>
+<body>
+<form method="post" action="javascript: void(0);" id="register-form" class="register-form" autocomplete="off" role="main">
+    <h1 class="a11y-hidden">Register Form</h1>
+    <div>
+        <label class="label-name">
+            <input type="text" class="text" name="name" placeholder="Name" tabindex="1" required />
+            <span class="required">Name</span>
+        </label>
+    </div>
+    <div>
+        <label class="label-email">
+            <input type="email" class="text" name="email" placeholder="Email" tabindex="2" required />
+            <span class="required">Email</span>
+        </label>
+    </div>
+    <input type="checkbox" name="show-password" class="show-password a11y-hidden" id="show-password" tabindex="3" />
+    <label class="label-show-password" for="show-password">
+        <span>Show Password</span>
+    </label>
+    <div>
+        <label class="label-password">
+            <input type="text" class="text" name="password" placeholder="Password" tabindex="3" required />
+            <span class="required">Password</span>
+        </label>
+    </div>
+    <input type="submit" value="Log In" />
+    <figure aria-hidden="true">
+        <div class="person-body"></div>
+        <div class="neck skin"></div>
+        <div class="head skin">
+            <div class="eyes"></div>
+            <div class="mouth"></div>
+        </div>
+        <div class="hair"></div>
+        <div class="ears"></div>
+        <div class="shirt-1"></div>
+        <div class="shirt-2"></div>
+    </figure>
+</form>
+</body>
+</html>
